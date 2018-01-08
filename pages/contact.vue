fef58317926d1be2a6d585b697268b029462415d
Joakim Gustin <jocke.gustin@gmail.com>
2018-01-08 20:30:36 +0100

    Full names (and more) on contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,20 @@
   <section class="section hero is-fullheight is-medium">
     <div class="container">
       <h1 class="title">Contact the team!</h1>
-      <p>There are a lot of people involved in making this happen but the core team consists of Rebecka G, Joakim G, Erik E, Caroline K, Piérre R, Maria W and Elin K.</p>
+      <p>There are a lot of people involved in making this happen but here are some of the heroes working day and night (for free) to make this happen:</p>
+      <p>
+        <ul>
+          <li>Rebecka Gustin</li>
+          <li>Joakim Gustin</li>
+          <li>Erik Edhagen</li>
+          <li>Caroline Kabat</li>
+          <li>Piérre Reimertz</li>
+          <li>Maria Westergaard</li>
+          <li>Elin Klemetz</li>
+          <li>Ninni Carlsson</li>
+          <li>Anna Thomasson</li>
+        </ul>
+      </p>
       <p>To get in touch with us, please drop an email to <a href="mailto:staff@metoohack.se">staff@metoohack.se</a></p>
       <p>Members of the media, please head to the <nuxt-link to="/press">the press page</nuxt-link>.</p>
     </div>
