5bad577f8c010605b70d513a9bf78d9936054b5f
Joakim Gustin <jocke.gustin@gmail.com>
2018-01-08 14:33:44 +0100

    Adding Maria W to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,7 @@
   <section class="section hero is-fullheight is-medium">
     <div class="container">
       <h1 class="title">Contact the team!</h1>
-      <p>There are a lot of people involved in making this happen but the core team consists of Rebecka G, Joakim G, Erik E, Caroline K, Piérre R and Elin K.</p>
+      <p>There are a lot of people involved in making this happen but the core team consists of Rebecka G, Joakim G, Erik E, Caroline K, Piérre R, Maria W and Elin K.</p>
       <p>To get in touch with us, please drop an email to <a href="mailto:staff@metoohack.se">staff@metoohack.se</a></p>
       <p>Members of the media, please head to the <nuxt-link to="/press">the press page</nuxt-link>.</p>
     </div>
