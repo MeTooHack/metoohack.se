ff0f4186cbe9b9ffdce5a5c35202adcfd085254f
Joakim Gustin <jocke.gustin@gmail.com>
2018-01-12 09:07:18 +0100

    Adding Stendahls to contributors

--- components/sections/Contributors.vue
+++ components/sections/Contributors.vue
@@ -5,6 +5,7 @@
       <div class="columns is-centered">
         <div class="column is-2"><a href="https://www.forthepublic.se/" target="_blank"><img src="~/assets/img/for_the_public_white.png" alt="For the Public" /></a></div>
         <div class="column is-2 is-offset-2"><a href="http://www.stinagranfors.se/" target="_blank"><img src="~/assets/img/stina-granfors.png" alt="Stina Gränfors Photography" /></a></div>
+        <div class="column is-2 is-offset-2"><a href="https://www.stendahls.se/" target="_blank"><img src="~/assets/img/stendahls.png" alt="Stendahls" /></a></div>
       </div>
     </div>
   </section>
